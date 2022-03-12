29eac4b7e4d07f9107bc534b9398fcff933af0af
Sup <suprabhatchowhan.96@gmail.com>
2022-03-12 18:34:06 +0530

    Now 2 die rolled

--- dice_roller.py
+++ dice_roller.py
@@ -1,7 +1,12 @@
 import random as r
 def main():
-  roll= r.randint(1,6)
-  print('You rolled a die of {}'.format(roll))
+  d_r= int(input("Enter the number of time wanna roll the dice:"))
+  d_s=0
+  for i in range(d_r):
+    roll= r.randint(1,6)
+    d_s+=roll
+    print('You rolled a die of {}'.format(roll))
+  print("You rolled a total dice of {}".format(d_s))
 
 if __name__== "__main__":
   main()
